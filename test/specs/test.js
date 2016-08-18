6d2b6a845515ff4c9c0d7b61f08305a4b1e94f1e
Peter Springer <peterspringer829@gmail.com>
2016-08-18 11:36:13 -0600

    adding int tests

--- test/specs/test.js
+++ test/specs/test.js
@@ -9,7 +9,13 @@ describe('welcome page', function(){
 });
 
 describe('input field', function(){
-  it('should have an input field for a message', function(){
+  it('should have an input field', function(){
+    browser.url('/');
+    var input = browser.element('.input-field');
+    assert.equal(input.isExisting(), true);
+  });
+
+  it('should have an input field that can hold a value', function(){
     browser.url('/');
     var input = browser.element('.input-field');
     input.setValue('hello');
@@ -18,6 +24,12 @@ describe('input field', function(){
 });
 
 describe('send button', function(){
+  it('should have a send button', function(){
+    browser.url('/');
+    var send = browser.element('#send-button');
+    assert.equal(send.isExisting(), true);
+  });
+
   it('should have a send button which is disabled if there is no input', function(){
     browser.url('/');
     var button = browser.element('#send-button');
@@ -38,19 +50,100 @@ describe('send button', function(){
   it('should append the message to the DOM', function(){
     browser.url('/');
     var input = browser.element('.input-field');
-    input.setValue('hello');
+    input.setValue('hello1');
     browser.click('#send-button');
-    assert.equal(browser.getText('.message'), 'hello');
+    assert.equal(browser.getText('.message'), 'hello1');
+    browser.click('.delete-button')
   });
 });
 
-  describe('delete button', function() {
-    it('should remove specific message when clicked', function() {
-      browser.url('/');
-      var input = browser.element('.input-field');
-      input.setValue('hello');
-      browser.click('#send-button');
-      browser.click('.delete-button');
-      assert.equal(browser.isExisting('.message'), false);
-    });
+describe('delete button', function() {
+  it('should have a delete button', function(){
+    browser.url('/');
+    var deleteButton = browser.element('.delete-button');
+    var input = browser.element('.input-field');
+    input.setValue('hello1');
+    browser.click('#send-button');
+    assert.equal(deleteButton.isExisting(), true);
+    browser.click('.delete-button');
   });
+
+  it('should remove message when clicked', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    input.setValue('hello1');
+    browser.click('#send-button');
+    browser.click('.delete-button');
+    assert.equal(browser.isExisting('.message'), false, 'The message is still on the page');
+  });
+});
+
+describe('messages on DOM', function() {
+  it('messages should exist after send is clicked', function(){
+    browser.url('/');
+    var input = browser.element('.input-field');
+    var message = browser.element('.message')
+    input.setValue('hello1');
+    browser.click('#send-button');
+    assert.equal(message.isExisting(), true);
+    browser.click('.delete-button');
+  });
+
+  it('should have a contenteditable attribute thats default is false', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    var message = browser.element('.message');
+    input.setValue('hello1');
+    browser.click('#send-button');
+    assert.equal(message.getAttribute('contenteditable'), "false");
+  });
+
+  it('content should become editable when you click the edit button', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    input.setValue('hello1');
+    var message = browser.element('.message');
+    browser.click('#send-button');
+    browser.click('.edit-button');
+    assert.equal(message.getAttribute('contenteditable'), "true");
+  });
+});
+
+
+describe('character counter', function() {
+  it('character count should exist on page load', function(){
+    browser.url('/');
+    var counter = browser.element('.counter')
+    assert.equal(counter.isExisting(), true);
+  });
+
+  it('should increase as characters are typed into the input field', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    var counter = browser.element('.counter');
+    input.setValue('hello1');
+    assert.equal(counter.getText(), 6);
+  });
+});
+
+describe('auto reply', function() {
+  it('should detect when an element is created/has text', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    var aimessage = browser.element('.message-ai');
+    input.setValue('hello1');
+    browser.click('#send-button');
+    assert.equal(aimessage.waitForText(2000), true);
+  });
+});
+
+describe('local storage', function() {
+  it('has data within local storage', function() {
+    browser.url('/');
+    var input = browser.element('.input-field');
+    input.setValue('hello1');
+    var message = browser.element('.message');
+    browser.click('#send-button');
+    assert.equal(browser.localStorageSize().value, 1);
+  });
+});
